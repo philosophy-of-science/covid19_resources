de5de9391cd1eee3ff6922c54165b3fc628ccf8e
Ryan Feigenbaum <director@philsci.org>
2021-02-18 14:26:09 -0500

    rename and remove div

---
+++ util/articles.js
@@ -0,0 +1,86 @@
+const axios = require("axios");
+const axiosCookieJarSupport = require("axios-cookiejar-support").default;
+const tough = require("tough-cookie");
+
+const fs = require("fs");
+const path = require("path");
+
+const jsonData = require("../_data/covid.json");
+
+axiosCookieJarSupport(axios);
+
+const cookieJar = new tough.CookieJar();
+
+const getImage = article => {
+  return new Promise(async (resolve, reject) => {
+    const { data } = await axios.get(article.link, {
+      jar: cookieJar,
+      withCredentials: true,
+    });
+    const img =
+      (data.match(/[^"]+\.jpg/) && data.match(/[^"]+\.jpg/)[0]) ||
+      "https://philsci.org/photos/covid.svg";
+    article.image = img;
+    resolve(article);
+    reject("error");
+  });
+};
+
+const articles = jsonData
+  .filter(item => item.type === "Article/Essay")
+  .sort((a, b) => (a.title > b.title ? 1 : -1));
+
+const findImagesForArticles = async articles => {
+  const fetchedImages = await Promise.all(
+    articles.map(article => getImage(article))
+  );
+  return fetchedImages;
+};
+
+const setMetadata = (articles, limit) => {
+  const metadata = articles.reduce((acc, cv) => {
+    const source = cv.source;
+    if (acc[source]) {
+      acc[source]++;
+    } else {
+      acc[source] = 1;
+    }
+    return acc;
+  }, {});
+
+  const segmentedArticles = articles.reduce((acc, cv) => {
+    const source = cv.source;
+    if (metadata[source] > limit) {
+      if (acc[source]) {
+        acc[source] = [...acc[source], cv];
+      } else {
+        acc[source] = [cv];
+      }
+    } else {
+      if (acc.articles) {
+        acc.articles = [...acc.articles, cv];
+      } else {
+        acc.articles = [cv];
+      }
+    }
+    return acc;
+  }, {});
+  return segmentedArticles;
+};
+
+const writeArticleData = async articles => {
+  const articlesWithImages = await findImagesForArticles(articles);
+  const articlesAndMetadata = setMetadata(articlesWithImages, 5);
+  fs.writeFile(
+    path.join("../_data/", "article.json"),
+    JSON.stringify(articlesAndMetadata),
+    err => {
+      if (err) console.log("error");
+      console.log("Article data written");
+    }
+  );
+};
+
+writeArticleData(articles);
+
+module.exports = findImagesForArticles;
